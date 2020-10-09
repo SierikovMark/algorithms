55b9c57dfac7a5540d7d31465fd4e1bd997930e3
Mark Sierikov <serikov.lg@gmail.com>
2020-10-09 12:17:53 +0300

    Added couple new solutions for RomanToInteger with N time complexity.

--- src/main/java/org/algorithms/leetcode/easy/RomanToInteger.java
+++ src/main/java/org/algorithms/leetcode/easy/RomanToInteger.java
@@ -1,8 +1,77 @@
 package org.algorithms.leetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToInteger {
 
     public static int romanToInt(String s) {
+        if (s == null) return -1;
+
+        int index = s.length() - 2;
+        char[] arr = s.toCharArray();
+        int result = getValue(arr[index + 1]);
+        while (index >= 0) {
+            if (getValue(arr[index]) < getValue(arr[index + 1])) {
+                result -= getValue(arr[index]);
+            } else {
+                result += getValue(arr[index]);
+            }
+            index--;
+        }
+        return result;
+    }
+
+    private static int getValue(char c) {
+        if (c == 'I') {
+            return 1;
+        } else if (c == 'V') {
+            return 5;
+        } else if (c == 'X') {
+            return 10;
+        } else if (c == 'L') {
+            return 50;
+        } else if (c == 'C') {
+            return 100;
+        } else if (c == 'D') {
+            return 500;
+        } else if (c == 'M') {
+            return 1000;
+        } else {
+            throw new IllegalArgumentException("Unrecognized character: " + c);
+        }
+    }
+
+    private final static Map<Character, Integer> VALUES = new HashMap<>();
+
+    static {
+        VALUES.put('I', 1);
+        VALUES.put('V', 5);
+        VALUES.put('X', 10);
+        VALUES.put('L', 50);
+        VALUES.put('C', 100);
+        VALUES.put('D', 500);
+        VALUES.put('M', 1000);
+    }
+
+    public static int romanToIntSecond(String s) {
+        if (s == null) return -1;
+
+        int index = s.length() - 2;
+        char[] arr = s.toCharArray();
+        int result = VALUES.get(arr[index + 1]);
+        while (index >= 0) {
+            if (VALUES.get(arr[index]) < VALUES.get(arr[index + 1])) {
+                result -= VALUES.get(arr[index]);
+            } else {
+                result += VALUES.get(arr[index]);
+            }
+            index--;
+        }
+        return result;
+    }
+
+    public static int romanToIntFirst(String s) {
         if (s == null) return -1;
 
         int index = s.length() - 1;
