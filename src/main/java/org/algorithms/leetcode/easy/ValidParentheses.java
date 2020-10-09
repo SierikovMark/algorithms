76eb7b1148fc4f4a6019887baed6844b63429f43
Mark Sierikov <serikov.lg@gmail.com>
2020-10-10 01:26:14 +0300

    [leetcode] (easy) ValidParentheses added additional solution without extra space, but with n^n time complexity (just for fun. I wondered if I can solve this without extra space).

--- src/main/java/org/algorithms/leetcode/easy/ValidParentheses.java
+++ src/main/java/org/algorithms/leetcode/easy/ValidParentheses.java
@@ -5,6 +5,33 @@ import java.util.Deque;
 
 public class ValidParentheses {
 
+
+    public static boolean isValidNoExtraSpaceButWithPowOfNTimeComplexityJustForFun(String s) {
+        if (s == null || s.length() % 2 != 0) return false;
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (isClosed(arr[i])) {
+                int j = i - 1;
+                int amountOfClosed = 0;
+                while (j >= 0 && arr[j] != getPair(arr[i])) {
+                    if (isClosed(arr[j])) {
+                        amountOfClosed++;
+                        j--;
+                        continue;
+                    }
+                    if (amountOfClosed > 0) {
+                        j--;
+                        amountOfClosed--;
+                        continue;
+                    }
+                    return false;
+                }
+                if (j == -1) return false;
+            }
+        }
+        return true;
+    }
+
     public static boolean isValid(String s) {
         if (s == null || s.length() % 2 != 0) return false;
         Deque<Character> deque = new ArrayDeque<>();
@@ -43,6 +70,7 @@ public class ValidParentheses {
     public static void main(String[] args) {
         System.out.println(isValid("[(())]"));
         System.out.println(isValid("]]]]]]]]"));
+        System.out.println(isValid("([[[[[[[]]]]]]]]"));
         System.out.println(isValid("{(})"));
     }
 }
