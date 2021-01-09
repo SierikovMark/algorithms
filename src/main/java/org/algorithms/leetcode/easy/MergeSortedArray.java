b550de477c5af0faaf8b260a4ca0c2415ef3d7eb
Mark Sierikov <serikov.lg@gmail.com>
2021-01-09 16:12:27 +0200

    [leetcode] Merge Sorted Array. First implementation: time complexity O(2n) ~ O(n). Space complexity O(2n). Main implementation: Time complexity O(n). Space complexity O(n+m). Fix print method.

---
+++ src/main/java/org/algorithms/leetcode/easy/MergeSortedArray.java
@@ -0,0 +1,59 @@
+package org.algorithms.leetcode.easy;
+
+import org.algorithms.utils.Utils;
+
+public class MergeSortedArray {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        verifyInput(nums1, m, n);
+
+        int i = m - 1; int j = n - 1;
+
+        int currentIndex = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j]) {
+                nums1[currentIndex--] = nums1[i--];
+            } else {
+                nums1[currentIndex--] = nums2[j--];
+            }
+        }
+
+        while (j >= 0) {
+            nums1[currentIndex--] = nums2[j--];
+        }
+    }
+
+    public static void mergeFirstImplementation(int[] nums1, int m, int[] nums2, int n) {
+        verifyInput(nums1, m, n);
+
+        int i = 0; int x = 0; int y = 0;
+
+        int[] result  = new int[nums1.length];
+
+        while (i < nums1.length) {
+            if (y >= n || nums1[x] <= nums2[y] && x < m) {
+                result[i] = nums1[x++];
+            } else {
+                result[i] = nums2[y++];
+            }
+            i++;
+        }
+
+        for(i = 0; i < result.length; i++) {
+            nums1[i] = result[i];
+        }
+    }
+
+    private static void verifyInput(int[] nums1, int m, int n) {
+        if (nums1.length != m + n) {
+            throw new IllegalArgumentException("Array nums1 doesn't have enough space");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1};
+        merge(nums1, 1, new int[] {0}, 0);
+        Utils.printIntArray(nums1, nums1.length);
+    }
+}
--- src/main/java/org/algorithms/utils/Utils.java
+++ src/main/java/org/algorithms/utils/Utils.java
@@ -3,7 +3,7 @@ package org.algorithms.utils;
 public class Utils {
 
     public static void printIntArray(int[] arr, int index) {
-        if (index >= arr.length) throw new IllegalArgumentException("Array length less then required index");
+        if (index > arr.length) throw new IllegalArgumentException("Array length less then required index");
         for (int i = 0; i < index; i++) {
             System.out.print(arr[i]);
             if (i != index - 1) {
