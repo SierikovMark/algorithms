4d4e846a72dfcf265646c67da3922d6fea9da1e9
Mark Sierikov <serikov.lg@gmail.com>
2021-04-18 00:48:11 +0300

    [leetcode] (easy) Added Convert Sorted Array to Binary Search Tree solutions. First solution traditioanlly is recursively approach with O(n) time complexity and O(n) extra space for new nodes. Second solution is iterative approach with same time complexity and O(2n) ~ O(n) extra space for keeping stacks for nodes and indexes.

---
+++ src/main/java/org/algorithms/leetcode/easy/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,81 @@
+package org.algorithms.leetcode.easy;
+
+import org.algorithms.leetcode.common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import static org.algorithms.utils.Utils.printBFTreeNode;
+
+public class ConvertSortedArrayToBinarySearchTree {
+
+    public static final int PLACEHOLDER = -1;
+
+    private static class Indexes {
+        int low;
+        int high;
+
+        public Indexes(int low, int high) {
+            this.low = low;
+            this.high = high;
+        }
+    }
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        Deque<TreeNode> nodeStack = new ArrayDeque<>();
+        Deque<Indexes> indexesStack = new ArrayDeque<>();
+        TreeNode rootNode = new TreeNode(PLACEHOLDER);
+        nodeStack.push(rootNode);
+        indexesStack.add(new Indexes(0, nums.length - 1));
+
+        while (!nodeStack.isEmpty()) {
+            TreeNode node = nodeStack.pop();
+            Indexes indexes = indexesStack.pop();
+            int mid = (indexes.low + indexes.high) / 2;
+            node.val = nums[mid];
+            if (indexes.low <= mid - 1) {
+                node.left = new TreeNode(PLACEHOLDER);
+                nodeStack.push(node.left);
+                indexesStack.push(new Indexes(indexes.low, mid - 1));
+            }
+            if (mid + 1 <= indexes.high) {
+                node.right = new TreeNode(PLACEHOLDER);
+                nodeStack.push(node.right);
+                indexesStack.push(new Indexes(mid + 1, indexes.high));
+            }
+        }
+        return rootNode;
+    }
+
+
+    public static TreeNode sortedArrayToBSTRecursive(int[] nums) {
+        return buildSubTree(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode buildSubTree(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int middle = (start + end) / 2;
+        TreeNode node = createNode(nums, middle);
+        node.left = buildSubTree(nums, start, middle - 1);
+        node.right = buildSubTree(nums, middle + 1, end);
+        return node;
+    }
+
+    private static TreeNode createNode(int[] nums, int index) {
+        return new TreeNode(nums[index]);
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode1 = sortedArrayToBST(new int[] {-10,-3,0,5,9});
+        printBFTreeNode(treeNode1);
+
+        TreeNode treeNode = sortedArrayToBSTRecursive(new int[] {-10,-3,0,5,9});
+        printBFTreeNode(treeNode);
+
+    }
+}
--- src/main/java/org/algorithms/utils/Utils.java
+++ src/main/java/org/algorithms/utils/Utils.java
@@ -1,6 +1,13 @@
 package org.algorithms.utils;
 
+import org.algorithms.leetcode.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Utils {
+
+    private final static int OFFSET_MOD_COUNT = 2;
 
     public static void printIntArray(int[] arr, int index) {
         if (index > arr.length) throw new IllegalArgumentException("Array length less then required index");
@@ -16,4 +23,51 @@ public class Utils {
     public static void printIntArray(int[] arr) {
         printIntArray(arr, arr.length);
     }
+
+    // TODO: needs some correction for node value length
+    public static void printBFTreeNode(TreeNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
+        int depth = findDepth(treeNode);
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(treeNode);
+
+        int leftOffset = OFFSET_MOD_COUNT * (depth - 1);
+        while (!queue.isEmpty()) {
+            printOffset(leftOffset);
+            int rowSize = queue.size();
+            while (rowSize-- > 0) {
+                TreeNode node = queue.poll();
+                System.out.print(node.val);
+                printOffset(OFFSET_MOD_COUNT + 1);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            System.out.println();
+            leftOffset -= OFFSET_MOD_COUNT;
+        }
+    }
+
+    private static void printOffset(int amountOfSpaces) {
+        System.out.print(" ".repeat(amountOfSpaces));
+    }
+
+    private static int findDepth(TreeNode treeNode) {
+        if (treeNode == null) {
+            return 0;
+        }
+        return 1 + Math.max(findDepth(treeNode.left), findDepth(treeNode.right));
+    }
 }
+
+//         1
+//       1   1
+//     1   1   1
+//   1   1   1   1
+// 1   1   1   1   1
