073b2531e3150e7ef3f9d300fcffa37b35981ae1
Mark Sierikov <serikov.lg@gmail.com>
2021-04-19 20:42:36 +0300

    [leetcode] (easy) added Pascal's Triangle II optimised by space solution. Time complexity is still O(n^2) but space complexity is O(k) where k is final length of result.

--- src/main/java/org/algorithms/leetcode/easy/PascalsTriangleII.java
+++ src/main/java/org/algorithms/leetcode/easy/PascalsTriangleII.java
@@ -5,6 +5,31 @@ import java.util.List;
 
 public class PascalsTriangleII {
 
+    public static List<Integer> getRowOptimisedBySpace(int rowIndex) {
+        List<Integer> result = new ArrayList<>();
+        result.add(1);
+
+        if (rowIndex < 1) {
+            return result;
+        }
+
+        for (int i = 1; i <= rowIndex; i++) {
+            result.add(0, 1);
+            for (int j = 1; j < result.size() - 1; j++) {
+                result.set(j, result.get(j) + result.get(j + 1));
+            }
+        }
+        return result;
+    }
+
+    // 1 1 2 1
+    // 1 3 3 1
+    // 1 1 3 3 1
+    // 1 4 6 4 1
+    // 1 1 4 6 4 1
+    // 1 5 10 10 5 1
+
+
     public static List<Integer> getRow(int rowIndex) {
         List<Integer> firstElement = new ArrayList<>();
         firstElement.add(1);
@@ -30,6 +55,6 @@ public class PascalsTriangleII {
     }
 
     public static void main(String[] args) {
-        System.out.println(getRow(3));
+        System.out.println(getRowOptimisedBySpace(5));
     }
 }
