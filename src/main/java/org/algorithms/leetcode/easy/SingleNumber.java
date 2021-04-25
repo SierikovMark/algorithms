d570bfbf9ea06399b7c7bc6a23609bae99c89f9f
Mark Sierikov <serikov.lg@gmail.com>
2021-04-26 00:13:58 +0300

    [leetcode] (easy) added another Single Number solution. Time complexity is O(n)  space complexity O(n).

--- src/main/java/org/algorithms/leetcode/easy/SingleNumber.java
+++ src/main/java/org/algorithms/leetcode/easy/SingleNumber.java
@@ -2,9 +2,25 @@ package org.algorithms.leetcode.easy;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class SingleNumber {
+
+    // Based on math 2 * (a + b) - (a + a + b) = b
+     public int singleNumber3(int[] nums) {
+         Set<Integer> setOfNums = new HashSet();
+         int sumOfSet = 0, sumOfNums = 0;
+         for (int num : nums) {
+             if (!setOfNums.contains(num)) {
+                 setOfNums.add(num);
+                 sumOfSet += num;
+             }
+             sumOfNums += num;
+         }
+         return 2 * sumOfSet - sumOfNums;
+     }
 
     public static int singleNumber2(int[] nums) {
         int length = nums.length;
