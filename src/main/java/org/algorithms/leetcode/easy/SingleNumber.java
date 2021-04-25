153ec24a4e270914fada957efb60dc14fe3eaf61
Mark Sierikov <serikov.lg@gmail.com>
2021-04-25 23:53:54 +0300

    [leetcode] (easy) added another Single Number solution. Time complexity is O(nlog n) no extra space.

--- src/main/java/org/algorithms/leetcode/easy/SingleNumber.java
+++ src/main/java/org/algorithms/leetcode/easy/SingleNumber.java
@@ -1,9 +1,27 @@
 package org.algorithms.leetcode.easy;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class SingleNumber {
+
+    public static int singleNumber2(int[] nums) {
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < length - 1; i+=2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        if (nums[length - 1] != nums[length - 2]) {
+            return nums[length - 1];
+        }
+        throw new RuntimeException("All nums have pair");
+    }
 
     public static int singleNumber(int[] nums) {
         Map<Integer, Integer> hash = new HashMap<>();
